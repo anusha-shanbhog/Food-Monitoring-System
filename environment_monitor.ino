6de4cbd89f9216425579d7a2ad1fcd268f8ed99e
Anusha Shanbhog <92755610+anusha-shanbhog@users.noreply.github.com>
2023-10-15 04:40:32 -0400

    Add files via upload

---
+++ environment_monitor.ino
@@ -0,0 +1,141 @@
+#define BLYNK_TEMPLATE_ID "TMPL3D4ua0u_8"
+#define BLYNK_TEMPLATE_NAME "food monitoring system"
+#define BLYNK_AUTH_TOKEN "rAE8kz5B8ZAXo66W6yHMGs0-vhSxjtbm"
+
+#define BLYNK_PRINT Serial
+#include <WiFi.h> 
+#include <BlynkSimpleEsp32.h>
+#include <DHT.h>
+#include <LiquidCrystal_I2C.h>
+
+LiquidCrystal_I2C lcd(0x3F, 16, 2);
+
+  byte degree_symbol[8] = 
+              {
+                0b00111,
+                0b00101,
+                0b00111,
+                0b00000,
+                0b00000,
+                0b00000,
+                0b00000,
+                0b00000
+              };
+              
+char auth[] = BLYNK_AUTH_TOKEN;
+
+char ssid[] = "Internet";  // type your wifi name
+char pass[] = "computer";  // type your wifi password
+
+BlynkTimer timer;
+
+int gas = 32;
+int sensorThreshold = 100;
+
+#define DHTPIN 2 //Connect Out pin to D2 in NODE MCU
+#define DHTTYPE DHT11  
+DHT dht(DHTPIN, DHTTYPE);
+
+
+void sendSensor()
+{
+  float h = dht.readHumidity();
+  float t = dht.readTemperature(); // or dht.readTemperature(true) for Fahrenheit
+  
+  if (isnan(h) || isnan(t)) {
+  Serial.println("Failed to read from DHT sensor!");
+  return;
+  }
+
+  int analogSensor = analogRead(gas);
+  Blynk.virtualWrite(V1, analogSensor);
+  Serial.print("Gas Value: ");
+  Serial.println(analogSensor);
+  if(analogSensor<1200){
+    Serial.println("Fresh Air");
+  }
+  if(analogSensor>1200){
+    Serial.println("Bad Air");
+  }
+  
+  // don't send more that 10 values per second.
+  Blynk.virtualWrite(V0, t);
+  Blynk.virtualWrite(V2, h);
+  Serial.print("Temperature : ");
+  Serial.print(t);
+  Serial.print("    Humidity : ");
+  Serial.println(h);
+}
+void setup()
+{   
+  
+  Serial.begin(115200);
+  
+ //pinMode(gas, INPUT);
+  Blynk.begin(auth, ssid, pass);
+  dht.begin();
+  timer.setInterval(5000L, sendSensor);
+
+  //Wire.begin();
+  lcd.begin();
+  lcd.clear();
+  lcd.backlight(); 
+
+  lcd.setCursor(3,0);
+  lcd.print("Air ");
+  lcd.setCursor(3,1);
+  lcd.print("Monitoring");
+  delay(2000);
+  lcd.clear();
+}
+
+void loop()
+{
+  Blynk.run();
+  timer.run();
+  float h = dht.readHumidity();
+  float t = dht.readTemperature(); // or dht.readTemperature(true) for Fahrenheit
+  int gasValue = analogRead(gas);
+  lcd.setCursor(0,0);
+  lcd.print("Temperature "); 
+  lcd.setCursor(0,1);
+  lcd.print(t);
+  lcd.setCursor(6,1);
+  lcd.write(1);
+  lcd.createChar(1, degree_symbol);
+  lcd.setCursor(7,1);
+  lcd.print("C");
+  delay(4000);
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Humidity ");
+  lcd.print(h);
+  lcd.print("%");
+  delay(4000);
+  lcd.clear();
+  
+  if(gasValue<1200)
+  {
+    lcd.setCursor(0,0);
+    lcd.print("Gas Value: ");
+    lcd.print(gasValue);
+    lcd.setCursor(0, 1);
+    lcd.print("Fresh Air");
+    //Serial.println("Fresh Air");
+    delay(4000);
+    lcd.clear();
+  }
+  else if(gasValue>1200)
+  {
+    lcd.setCursor(0,0);
+    lcd.print(gasValue);
+    lcd.setCursor(0, 1);
+    lcd.print("Bad Air");
+    //Serial.println("Bad Air");
+    delay(4000);
+    lcd.clear();
+    Blynk.logEvent("pollution_alert","Bad Air");
+  }    
+}
+ 
+ 
